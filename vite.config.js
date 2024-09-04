4b3041a768b0639bfad69b979e4de9620c01ad6d
chihiro kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2024-09-04 18:33:21 +0900

    build settings mod

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base:"/iike",
+  base:"/demo202409",
   build: {
-    outDir:"docs",
+    //outDir:"docs",
     //minify:false
   },
   //optimizeDeps:{
