c5a3a36965770e2f3e6d12b4ba2d300949609d7d
c-kikuchi <c-kikuchi@hi.u-tokyo.ac.jp>
2024-09-04 16:32:37 +0900

    mod widgets

--- src/viewer/components/widgets.js
+++ src/viewer/components/widgets.js
@@ -30,12 +30,34 @@ function simpleCommentingWidget(obj){
 function IDShowingWidget(obj){
   const elm = document.createElement("div");
   if(obj.annotation.id){
-    elm.innerText = "ID: " + obj.annotation.id;
+    const id = obj.annotation.id;
+
+    const ipt = document.createElement("input");
+    ipt.setAttribute("type", "text");
+    ipt.readOnly = true;
+    ipt.value = id;
+    ipt.style.cssText= `
+      width: 320px;
+      background-color: inherit;
+      color: #333;
+      border: 1px solid #999;
+    `;
+    ipt.addEventListener("focus", e=>e.target.select());
+    
+    const btn = document.createElement("button");
+    btn.setAttribute("type","button");
+    btn.innerHTML ="📋";
+    btn.title="Copy ID";
+    btn.addEventListener("click", e=>{
+      navigator.clipboard.writeText(id);
+    })
+
+    elm.append("ID: ", ipt, btn);
     elm.style.cssText = `
-      font-size:x-small;
-      color:#666;
+      font-size:small;
+      color:#333;
       background-color:#ccc;
-      padding-left:15px;
+      padding-left:5px;
       line-height:1.8em;
     `
   }
@@ -199,8 +221,8 @@ function LabeledCommentWidgetBuilder(_label, bridge){
     const current_value = current_body ? (label_exp.exec(current_body.value)?RegExp.$1:"") : "";
 
     const has_tag = !!obj.annotation && obj.annotation.bodies.some(body=>body.purpose == "tagging");
-    const has_describing =  !!obj.annotation &&
-      obj.annotation.bodies.some(body=>body.purpose=="describing");
+    /*const has_describing =  !!obj.annotation &&
+      obj.annotation.bodies.some(body=>body.purpose=="describing");*/
 
     const add_comment = function(e){
       if(current_body){
@@ -210,9 +232,7 @@ function LabeledCommentWidgetBuilder(_label, bridge){
         obj.onAppendBody(create_describing_body(label, e.target.value));
       }
     };
-    if(bridge.meta.is_taggingmode){
-      label = label + "";
-    }
+    
     
     container.className = "r6o-widget comment editable ii-comment";
     container.style.cssText = `
@@ -225,58 +245,59 @@ function LabeledCommentWidgetBuilder(_label, bridge){
       container.style.display = "none";
       return container;
     }
+    else{
+      const label_elm = document.createElement("div");
+      label_elm.innerText = label;
+      label_elm.style.cssText = `
+        width:80px;
+        background-color:#666;
+        color:#fff;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+      `;
 
-    const label_elm = document.createElement("div");
-    label_elm.innerText = label;
-    label_elm.style.cssText = `
-      width:80px;
-      background-color:#666;
-      color:#fff;
-      display:flex;
-      justify-content:center;
-      align-items:center;
-    `;
-
-    const input_elm = document.createElement("textarea");
-    input_elm.className = "r6o-editable-text";
-    input_elm.rows = "1";
-    input_elm.value = current_value;
-    const textarea_default_height = function(el){
-      el.style.height = "46px";
-    };
-    const textarea_auto_height = function(el){
-      el.style.height = el.scrollHeight + "px"; 
-    };
-    input_elm.addEventListener("change", add_comment); 
-    input_elm.addEventListener("input", e=>textarea_auto_height(e.target));
-    input_elm.addEventListener("focus", e=>textarea_auto_height(e.target));
-    input_elm.addEventListener("blur", e=>textarea_default_height(e.target));
-    input_elm.addEventListener("keyup", e=>e.key=="Delete"&&e.stopPropagation());
-    textarea_default_height(input_elm);
+      const input_elm = document.createElement("textarea");
+      input_elm.className = "r6o-editable-text";
+      input_elm.rows = "1";
+      input_elm.value = current_value;
+      const textarea_default_height = function(el){
+        el.style.height = "46px";
+      };
+      const textarea_auto_height = function(el){
+        el.style.height = el.scrollHeight + "px"; 
+      };
+      input_elm.addEventListener("change", add_comment); 
+      input_elm.addEventListener("input", e=>textarea_auto_height(e.target));
+      input_elm.addEventListener("focus", e=>textarea_auto_height(e.target));
+      input_elm.addEventListener("blur", e=>textarea_default_height(e.target));
+      input_elm.addEventListener("keyup", e=>e.key=="Delete"&&e.stopPropagation());
+      textarea_default_height(input_elm);
 
 
-    const searcher_elm = document.createElement("div");
-    searcher_elm.style.cssText = `
-      width:30px;
-      display:flex;
-      justify-content:center;
-      align-items:center;
-    `;
-    const searcher_btn = document.createElement("button");
-    searcher_btn.setAttribute("type", "button");
-    searcher_btn.innerHTML = "🔍&#xFE0E;";
-    searcher_btn.title = "索引型で検索";
-    searcher_btn.addEventListener("click", e=>{
-      const search_word = input_elm.value;
-      const search_url = "https://wwwap.hi.u-tokyo.ac.jp/ships/w30/search";
-      const search_query = `?keyword=${search_word}&book=井伊家史料&searchtarget=索引型&expand=true&type=2&page=1&itemsperpage=200&sortby=title_word roll_page&sortdesc=false&sortitem=見出し語：昇順`;
-      const url = search_url + encodeURI(search_query);
-      window.open(url);
-    });
-    searcher_elm.append(searcher_btn);
-    container.append(label_elm, input_elm, searcher_elm);
-    
-    return container;
+      const searcher_elm = document.createElement("div");
+      searcher_elm.style.cssText = `
+        width:30px;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+      `;
+      const searcher_btn = document.createElement("button");
+      searcher_btn.setAttribute("type", "button");
+      searcher_btn.innerHTML = "🔍&#xFE0E;";
+      searcher_btn.title = "索引型で検索";
+      searcher_btn.addEventListener("click", e=>{
+        const search_word = input_elm.value;
+        const search_url = "https://wwwap.hi.u-tokyo.ac.jp/ships/w30/search";
+        const search_query = `?keyword=${search_word}&book=井伊家史料&searchtarget=索引型&expand=true&type=2&page=1&itemsperpage=200&sortby=title_word roll_page&sortdesc=false&sortitem=見出し語：昇順`;
+        const url = search_url + encodeURI(search_query);
+        window.open(url);
+      });
+      searcher_elm.append(searcher_btn);
+      container.append(label_elm, input_elm, searcher_elm);
+      
+      return container;
+    }
   }
 }
 
@@ -404,21 +425,68 @@ function IIKanPageWidgetBuilder(bridge){
 
 function IILinkingWidget(obj){
   const container = document.createElement("div");
-  const link_body = obj.annotation.bodies.find(body=>body.purpose=="linking");
-  if(link_body){
-    const link = document.createElement("a");
-    link.href=link_body.value;
-    link.target="_blank";
-    link.innerText="近世史編纂支援データベース";
-    const p = document.createElement("div");
-    p.append("🔗",link);
-    p.style.fontSize="small";
-    container.append(p);
-  }
+  container.style.cssText = `
+    display:flex;
+    flex-direction:row;
+  `;
+  const link_container = document.createElement("div");
+  link_container.style.cssText =`
+    flex-grow:1;
+  `;
+  const link_bodies = obj.annotation.bodies.filter(body=>body.purpose=="linking");
+  link_bodies.forEach(link_body=>{
+    if(link_body){
+      const url = link_body.value;
+      const link = document.createElement("a");
+      link.href = url;
+      link.target="_blank";
+      if(url.match(/^https\:\/\/wwwap\.hi\.u\-tokyo\.ac\.jp\/ships\/w30/)){
+        link.innerText="近世史編纂支援データベース";
+      }
+      else{
+        link.innerText = url.slice(0, 30);
+      }
+      const btn = document.createElement("button");
+      btn.innerText="Edit"
+      btn.addEventListener("click",e=>{
+        let new_url = prompt("input new url",url);
+        if(new_url && new_url!=url){
+          if((/^\d{8}$/).test(new_url)){
+            new_url = `https://wwwap.hi.u-tokyo.ac.jp/ships/w30/detail/3001${new_url}?dispid=disp02&type=2`
+          }
+          obj.onUpdateBody(link_body,{
+            "type": "TextualBody",
+            "purpose": "linking",
+            "value": new_url
+          });
+        }
+      })
+      const p = document.createElement("div");
+      p.append("🔗",link,btn);
+      p.style.fontSize="small";
+      link_container.append(p);
+    }
+  })
+  const append_btn = document.createElement("button");
+  append_btn.innerText = "+";
+  append_btn.addEventListener("click",e=>{
+    let new_url = prompt("input new url");
+    if(new_url){
+      if((/^\d{8}$/).test(new_url)){
+        new_url = `https://wwwap.hi.u-tokyo.ac.jp/ships/w30/detail/3001${new_url}?dispid=disp02&type=2`
+      }
+      obj.onAppendBody({
+        "type": "TextualBody",
+        "purpose": "linking",
+        "value": new_url
+      });
+    }
+  })
+  container.append(link_container,append_btn);
   return container;
 }
 
-function apply_candidate(obj, cand){
+function apply_candidate(obj, cand, join_mode = false){
   const annot = obj.annotation.underlying;
   const diffs = [];
   describing_keys.forEach(key=>{
@@ -426,11 +494,23 @@ function apply_candidate(obj, cand){
     if(annot["_type"]=="describing" || obj.annotation.bodies.every(body=>body.purpose!="tagging")){
       const current_body = obj.annotation.bodies.find(body=>body.purpose=="describing"&&(new RegExp(`^${key}: `)).test(body.value));
       if(current_body){
-        diffs.push({
-          action:"update",
-          previous:current_body,
-          updated:create_describing_body(key, dat_value)
-        });
+        if(join_mode && ["巻","頁","番号","枝番"].every(k=>k!=key)){
+          const label_exp = new RegExp(`^${key}: ([\\s\\S]*)$`);
+          const current_value = current_body ? (label_exp.exec(current_body.value)?RegExp.$1:"") : "";
+          const new_value = ((current_value&&current_value!=dat_value)?(current_value+ "・"):"")  + dat_value;
+          diffs.push({
+            action:"update",
+            previous:current_body,
+            updated:create_describing_body(key, new_value)
+          });
+        }
+        else{
+          diffs.push({
+            action:"update",
+            previous:current_body,
+            updated:create_describing_body(key, dat_value)
+          });
+        }
       }
       else{
         diffs.push({
@@ -447,7 +527,7 @@ function apply_candidate(obj, cand){
       "value": `https://wwwap.hi.u-tokyo.ac.jp/ships/w30/detail/3001${cand.data["DB-ID"]}?dispid=disp02&type=2`
     };
     const link_body = obj.annotation.bodies.find(body=>body.purpose=="linking");
-    if(link_body){
+    if(link_body && !join_mode){
       diffs.push({
         action:"update",
         previous:link_body,
@@ -465,11 +545,34 @@ function apply_candidate(obj, cand){
     obj.onBatchModify(diffs);
   }
 }
+function join_candidate(obj, cand){
+  apply_candidate(obj,cand,true);
+}
+function create_candidate(obj){
+  const annot = obj.annotation.underlying;
+  const cand = {
+    title:"",
+    data:{}
+  }
+  if(annot["_type"]=="describing" || obj.annotation.bodies.every(body=>body.purpose!="tagging")){
+    describing_keys.forEach(key=>{
+      const current_body = obj.annotation.bodies.find(body=>body.purpose=="describing"&&(new RegExp(`^${key}: `)).test(body.value));
+      const label_exp = new RegExp(`^${key}: ([\\s\\S]*)$`);
+      const current_value = current_body ? (label_exp.exec(current_body.value)?RegExp.$1:"") : "";
+      cand.data[key] = current_value;
+    });
+    const link_body = obj.annotation.bodies.find(body=>body.purpose=="linking"&&(/https\:\/\/wwwap\.hi\.u\-tokyo\.ac\.jp\/ships\/w30\/detail\/3001(\d{8})\?dispid=disp02&type=2/).test(body.value));
+    if(link_body){
+      cand.data["DB-ID"] = RegExp.$1;
+    }
+  }
+  return cand;
+}
 
 function candidateSelectorWidget(obj){
   const container = document.createElement("div");
   const annot = obj.annotation.underlying;
-  if(annot["_candidate"]&&annot["_candidate"].length>0){
+  if(annot["_candidate"] && annot["_candidate"].length>0){
     annot["_candidate"].forEach(cand=>{
       const wrapper = document.createElement("div");
       wrapper.style.cssText = `
@@ -493,14 +596,50 @@ function candidateSelectorWidget(obj){
       const apply_button = document.createElement("button");
       apply_button.setAttribute("type", "button");
       apply_button.innerText = "Apply";
-      apply_button.addEventListener("click", ()=>apply_candidate(obj, cand))
-      btn_wrapper.append(apply_button);
+      apply_button.addEventListener("click", ()=>{
+        apply_candidate(obj, cand)
+      });
+      const join_button = document.createElement("button");
+      join_button.setAttribute("type", "button");
+      join_button.innerText ="Join";
+      join_button.addEventListener("click",e=>{
+        join_candidate(obj, cand);
+      });
+      const copy_btn = document.createElement("button");
+      copy_btn.innerText = "◎";
+      copy_btn.addEventListener("click", e=>{
+        navigator.clipboard.writeText(JSON.stringify(cand));
+      })
+      btn_wrapper.append(apply_button,join_button,copy_btn);
 
       wrapper.append(details,btn_wrapper);
       container.append(wrapper);
     })
   }
-
+  if(annot["_type"]=="describing" || obj.annotation.bodies.every(body=>body.purpose!="tagging")){
+    const btnwrapper = document.createElement("div");
+    btnwrapper.style.cssText = `text-align:right`;
+    const paste_btn = document.createElement("button");
+    paste_btn.innerText = "Paste Data";
+    paste_btn.addEventListener("click",e=>{
+      const text = prompt("input data json","");
+      if(text){
+        let json = JSON.parse(text);
+        if(typeof json.title == "undefined"){
+          json = {title:"",data:json};
+        }
+        apply_candidate(obj, json);
+      }
+    })
+    const candcopy_btn = document.createElement("button");
+    candcopy_btn.innerText = "Copy Data";
+    candcopy_btn.addEventListener("click", e=>{
+      const cand = create_candidate(obj);
+      navigator.clipboard.writeText(JSON.stringify(cand));
+    })
+    btnwrapper.append(paste_btn, candcopy_btn);
+    container.append(btnwrapper);
+    }
   return container;
 }
 
